5c29e96e095b8ca1de872233dd43e489a3a69409
Nolan Nguyen! <nolan2087@gmail.com>
2024-05-24 10:56:15 -0700

    playing around with routing and pages. getting the hang of it

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Hey there, ViDoc Dashboard!</h1>
+            <Link href="/">Click here to go back.</Link>
+        </div>
+    );
+}
