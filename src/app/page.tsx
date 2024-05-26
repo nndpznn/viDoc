a428ab183d66b10c1353c6a9eadaea0f87cf52df
Nolan Nguyen! <nolan2087@gmail.com>
2024-05-26 16:37:13 -0700

    adding supabase url and keys to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import supabase from './config/supabaseClient'
 
 export default function Page() {
-    return (
-        <div>
-            <h1>You are Home.</h1>
-            <Link href="/dashboard">Click here to navigate to Dashboard!</Link>
-        </div>
-    );
+  //   console.log(supabase)
+
+  return (
+    <div>
+      <h1>You are Home.</h1>
+      <Link href="/dashboard">Click here to navigate to Dashboard!</Link>
+    </div>
+  )
 }
