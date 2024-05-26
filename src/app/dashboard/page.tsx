641bc3da41cc5a24dfa8da84567e9a4b442ef358
Masao Kitamura <masaok@gmail.com>
2024-05-26 11:48:56 +0900

    Adding prettier and VSCode workspace settings

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 
 export default function Page() {
-    return (
-        <div>
-            <h1>Hey there, ViDoc Dashboard!</h1>
-            <Link href="/">Click here to go back.</Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hey there, ViDoc Dashboard!</h1>
+      <Link href="/">Click here to go back.</Link>
+    </div>
+  )
 }
