8eefb3d71659e04ad45d46bb2c0ee6a243918ad6
Nolan Nguyen! <nolan2087@gmail.com>
2024-05-29 23:33:08 -0700

    overriding default mui button colors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,9 +5,17 @@ import { useRouter } from 'next/navigation'
 import Image from 'next/legacy/image'
 
 import { Avatar, CircularProgress, IconButton, Button, Box, Container, Grid, Card } from '@mui/material'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { grey } from '@mui/material/colors'
 
 import { supabase } from '@/clients/supabaseClient'
 import { useSupabaseUserMetadata } from '@/hooks/useSupabaseUserMetadata'
+
+const theme = createTheme({
+  palette: {
+    primary: grey,
+  },
+})
 
 export default function Dashboard() {
   const router = useRouter()
@@ -45,39 +53,41 @@ export default function Dashboard() {
 
       <br></br>
 
-      <Grid container spacing={2} className="flex justify-center content-center">
-        <Grid item xs={'auto'}>
-          <Button variant="contained" size="large" color="primary">
-            Create Project
-          </Button>
+      <ThemeProvider theme={theme}>
+        <Grid container spacing={2} className="flex justify-center content-center">
+          <Grid item xs={'auto'}>
+            <Button variant="contained" size="large" color="primary">
+              Create Project
+            </Button>
+          </Grid>
+          <Grid item xs={'auto'}>
+            <Button variant="contained" size="large" color="primary">
+              View Projects
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={'auto'}>
-          <Button variant="contained" size="large" color="primary">
-            View Projects
-          </Button>
-        </Grid>
-      </Grid>
+      </ThemeProvider>
 
       <br></br>
 
       <Grid container spacing={4} className="flex justify-center content-center">
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
         <Grid item xs={6}>
-          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="350" height="200" />
+          <Image src="/images/Not-Found.png" alt="ViDoc Logo" layout="responsive" width="300" height="170" />
         </Grid>
       </Grid>
     </div>
